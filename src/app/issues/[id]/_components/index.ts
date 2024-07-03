6f85fb4075e610a774115105bb6df3e0a422ff39
Bartłomiej Marzec <bmarzec@sii.pl>
2024-07-03 23:33:42 +0200

    added edit button and refactor

--- src/app/issues/[id]/_components/index.ts
+++ src/app/issues/[id]/_components/index.ts
@@ -1,1 +1,3 @@
-export { LinkHandler } from "./LinkHandler";
+export { LinkHandler } from "./link-handler";
+export { EditIssueButton } from "./edit-issue-button";
+export { IssueDetails } from "./issue-details";
--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,10 +1,7 @@
-import React from "react";
 import { notFound } from "next/navigation";
-import { MDXRemote } from "next-mdx-remote/rsc";
 
 import prisma from "@/lib/db";
-import { IssueStatus } from "../_components";
-import { LinkHandler } from "./_components";
+import { EditIssueButton, IssueDetails } from "./_components";
 
 interface IssueDetailsProps {
   params: {
@@ -28,22 +25,13 @@ async function IssueDetailsPage({ params }: IssueDetailsProps) {
   }
 
   return (
-    <div className="space-y-4 max-w-xl">
-      <h2 className="text-3xl font-bold">{issue.title}</h2>
-      <div className="flex space-x-3">
-        <IssueStatus status={issue.status} />
-        <p className="text-lg">{issue.createdAt.toDateString()}</p>
+    <div className="md:grid grid-cols-2">
+      <div>
+        <IssueDetails issue={issue} />
       </div>
-      <article className="border p-3 rounded-md prose lg:prose-xl">
-        <MDXRemote
-          source={issue.description}
-          components={{
-            a: ({ href, children }) => (
-              <LinkHandler href={href}>{children}</LinkHandler>
-            ),
-          }}
-        />
-      </article>
+      <div className="max-md:mt-5">
+        <EditIssueButton issueId={issue.id} />
+      </div>
     </div>
   );
 }
